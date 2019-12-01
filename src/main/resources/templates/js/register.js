734e9606a279fba430ef9e07b0788dcbf419d5e3
Douglas Andrei de lima <douglasandreilima@gmail.com>
2019-12-01 20:06:52 -0200

    Added Ajax request to search locale for zip code

--- src/main/resources/templates/js/register.js
+++ src/main/resources/templates/js/register.js
@@ -17,3 +17,28 @@ $("#registerForm").submit(function(event){
         }
     })
 });
+
+$(document).ready(function(){
+    
+});
+
+$( "#cep").blur(function() {
+    var urlCep = $('#cep').val();
+    var action = 'https://viacep.com.br/ws/'+urlCep+'/json/';
+    $.ajax({
+        url: action,
+        type: 'GET',
+        success: (response) => {
+            writeFields(response);
+        },
+        error: (errors) => {
+            alert('Erro ao buscar endereços pelo Cep, verifique sua conexão')
+        }
+      })
+  });
+
+function writeFields(response) {
+    $('#logradouro').val(response.logradouro)
+    $('#cidade').val(response.localidade)
+    $('#estado').val(response.uf)
+}
