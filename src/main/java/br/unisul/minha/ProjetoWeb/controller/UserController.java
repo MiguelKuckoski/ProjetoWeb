4f9aa33f4da5a8426bf7390329cf035b9ffd44b3
Douglas Andrei de lima <douglasandreilima@gmail.com>
2019-12-01 20:24:27 -0200

    Initial commit for function register user

--- src/main/java/br/unisul/minha/ProjetoWeb/controller/UserController.java
+++ src/main/java/br/unisul/minha/ProjetoWeb/controller/UserController.java
@@ -1,48 +1,66 @@
 package br.unisul.minha.ProjetoWeb.controller;
 
-import br.unisul.minha.ProjetoWeb.controller.helper.ControllerHelper;
-import br.unisul.minha.ProjetoWeb.model.Address;
-import br.unisul.minha.ProjetoWeb.model.User;
-import br.unisul.minha.ProjetoWeb.repositories.UserRepository;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.List;
+import br.unisul.minha.ProjetoWeb.model.User;
+import br.unisul.minha.ProjetoWeb.repositories.UserRepository;
 
 @Controller
 public class UserController {
 
-    @Autowired
-    private UserRepository userRepository;
+	@Autowired
+	private UserRepository userRepository;
 
-    @PostMapping(value = "/register")
-    public ResponseEntity register(@RequestBody @Validated User newUser, @Validated Address newAdress, BindingResult result) {
-        User user = userRepository.findByLogin(newUser.getLogin());
-        if (user != null)
-            return ResponseEntity.badRequest().body("Usuário já cadastrado.");
-        if (result.hasErrors()) {
-            String errors = ControllerHelper.getErrors(result);
-            return ResponseEntity.status(505).body(errors);
-        }
-        userRepository.save(newUser);
-        return ResponseEntity.ok().body("Cadastro realizado com sucesso.");
-    }
+//    @PostMapping(value = "/register")
+//    public ResponseEntity register(@RequestBody @Validated User newUser, @Validated Address newAdress, BindingResult result) {
+//        User user = userRepository.findByLogin(newUser.getLogin());
+//        if (user != null)
+//            return ResponseEntity.badRequest().body("Usuário já cadastrado.");
+//        if (result.hasErrors()) {
+//            String errors = ControllerHelper.getErrors(result);
+//            return ResponseEntity.status(505).body(errors);
+//        }
+//        userRepository.save(newUser);
+//        return ResponseEntity.ok().body("Cadastro realizado com sucesso.");
+//    }
 
-    @GetMapping("/register")
-    public String register() { return "/register"; }
+	@PostMapping("/register")
+	public ModelAndView registrar(@Valid User user, BindingResult result) {
+		ModelAndView mv = new ModelAndView();
+		User usr = userRepository.findByLogin(user.getLogin());
+		if (usr != null) {
+			result.rejectValue("email", "", "Usuário já cadastrado");
+		}
 
-    @GetMapping("/login")
-    public String login() {
-        return "/login";
-    }
+		if (result.hasErrors()) {
+			mv.setViewName("/register");
+			mv.addObject("usuario", user);
+		} else {
+			userRepository.save(user);
+			mv.setViewName("redirect:/login");
+		}
+		return mv;
+	}
 
-    @GetMapping("/logout")
-    public String logout() {
-        return "/login";
-    }
+	@GetMapping("/register")
+	public String register() {
+		return "/register";
+	}
+
+	@GetMapping("/login")
+	public String login() {
+		return "/login";
+	}
+
+	@GetMapping("/logout")
+	public String logout() {
+		return "/login";
+	}
 }
--- src/main/resources/templates/js/register.js
+++ src/main/resources/templates/js/register.js
@@ -1,23 +1,3 @@
-$("#registerForm").submit(function(event){
-    event.preventDefault();
-    var post_url = $(this).attr("action");
-    var request_method = $(this).attr("method");
-    var form_data = $(this).serialize();
-
-    $.ajax({
-        url : post_url,
-        type: request_method,
-        data : form_data,
-        timeout: 5000,
-        success: function(data, textStatus ){
-            alert('request successful');
-        },
-        fail: function(xhr, textStatus, errorThrown){
-            alert('request failed');
-        }
-    })
-});
-
 $(document).ready(function(){
     
 });
